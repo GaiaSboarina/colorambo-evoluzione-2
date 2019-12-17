691045d42e2f3a6da6990c893ab45caea4cf8dcf
Filippo799 <55846120+Filippo799@users.noreply.github.com>
2019-12-17 16:20:04 +0100

    back-end
    connessione database, aggiunta utente, ritornare array di colori o utenti dal database, aggiunta colori, login

--- functions.php
+++ functions.php
@@ -3,20 +3,20 @@
 //connessione al dbms
 function getConnection() {
     $connection = mysqli_connect("localhost", "root", "");
-    if ($connection) {
-        echo "Connessione al database riuscita<br>";
-    } else {
+    if (!$connection) {
         die("Connessione non riuscita<br>");
-    }
+    } /*else {
+        echo "Connessione al database riuscita<br>";
+    }*/
     return $connection;
 }
 
 //aggiunge un utente alla tabella degli utenti
 function addUtente($conn, $nome, $cognome, $nascita, $residenza, $descrizione, $email, $password, $account) {
     $query = "INSERT INTO utenti 
-    (nome, cognome, nascita, residenza, descrizione, email, password, account) 
-    VALUES
-    ('$nome', '$cognome', '$nascita', '$residenza', '$descrizione', '$email', '$password', '$account')";
+            (nome, cognome, nascita, residenza, descrizione, email, password, account) 
+            VALUES
+            ('$nome', '$cognome', '$nascita', '$residenza', '$descrizione', '$email', '$password', '$account')";
     if (mysqli_query($conn, $query)) {
         echo "Inserito utente $email<br>";
     } else {
@@ -24,7 +24,44 @@ function addUtente($conn, $nome, $cognome, $nascita, $residenza, $descrizione, $
     }
 }
 
+//prende dati da una tabella
+function getDati($query) {
+    $connection = getConnection();
+    $result = mysqli_query($connection, $query);
+    $dati = [];
+    if ($result) {
+        $dati = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    }
+    return $dati;
+}
+
+//aggiungere colore
+function addColor($nome, $codice, $creatore) {
+    $connection = getConnection();
+    $creatori = getDati("SELECT * FROM utenti");
+    if (in_array($creatore, $creatori)) {
+        $query = "INSERT INTO colori (nome, codice, creatore)
+                VALUES ('$nome', '$codice', '$creatore'";
+        if (mysqli_query($connection, $query)) {
+            echo "Inserito colore $nome<br>";
+        } else {
+            die("Errore nell'inserimento del colore $nome<br>");
+        }
+    } else {
+        echo "Il creatore $creatore non esiste";
+    }
+}
+
+//login
+function login($email, $password) {
+    $dati = getDati("SELECT email, password FROM utenti WHERE email = '$email' AND password = '$password'");
+    if (count($dati) > 0) {
+        echo "login";
+    }
+    else {
+        echo "utente non registrato";
+    }
+}
 
 
-//fatto: connessione database, aggiunta utente
 ?>
